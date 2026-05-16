811b93f11521586db8d1ef22f28a9fda6571f7a0
pratiksapkal333 <pratiksapkal333@gmail.com>
2026-05-16 22:31:19 +0530

    Created socket and implimented bind(), listen(), accept() and client connection

--- src/Server.cpp
+++ src/Server.cpp
@@ -1,6 +1,9 @@
 #include "../include/Server.hpp"
-
 #include <iostream>
+// Linux networking headers
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
 
 Server::Server(int p)
 {
@@ -10,7 +13,57 @@ Server::Server(int p)
 
 bool Server::start()
 {
-    std::cout << "Server starting on port " << port << std::endl;
+    // Create socket
+    serverSocket = socket(AF_INET, SOCK_STREAM, 0);
 
+    // Checks if socket creation is failed
+    if (serverSocket < 0)
+    {
+        std::cerr << "Socket creation failed" << std::endl;
+        return false;
+    }
+
+    // Prevent bind errors on restart
+    int opt = 1;
+    setsockopt(serverSocket, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+    std::cout << "Socket created successfully" << std::endl;
+
+    // Configure server address
+    sockaddr_in serverAddress;
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_port = htons(port);
+    serverAddress.sin_addr.s_addr = INADDR_ANY;
+
+    // Bind socket to port
+    if (bind(serverSocket, (sockaddr *)&serverAddress, sizeof(serverAddress)) < 0)
+    {
+        std::cerr << "Bind failed" << std::endl;
+        return false; // notify if bind failed
+    }
+    std::cout << "Bind Successful" << std::endl;
+
+    // Start listening
+    if (listen(serverSocket, 5) < 0)
+    {
+        std::cerr << "Listen Failed" << std::endl;
+        return false; // notify if listen failed
+    }
+    std::cout << "Server listening on port" << port << std::endl;
+
+    // Accept one client
+    sockaddr_in clientAddress;
+    socklen_t clientSize = sizeof(clientAddress);
+    int clientSocket = accept(serverSocket, (sockaddr *)&clientAddress, &clientSize);
+
+    if (clientSocket < 0)
+    {
+        std::cerr << "Client accept failed" << std::endl;
+        return false; // notify if accept failed
+    }
+    std::cout << "Client connected" << std::endl;
+
+    // close sockets
+    close(clientSocket);
+    close(serverSocket);
     return true;
 }
